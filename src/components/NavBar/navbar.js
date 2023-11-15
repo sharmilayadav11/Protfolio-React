7df1f07d5e0666369b537bfd3b298089c0c81b61
sharmila <48220@edu.raseko.fi>
2023-11-15 23:19:03 +0200

    My very first react project to create a portfolio

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react' //rafce will write component function automati...
+import "./navbar.css"
+
+const Navbar = () => {
+    return (
+        <div>Navbar</div>
+    )
+}
+
+export default Navbar
---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+//https://www.youtube.com/watch?v=4ag1LsgIUc0&t=281&ab_channel=GreatStack
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
